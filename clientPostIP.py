7a8431dba5f7118f6e16a95104c72ec278f00b37
alexnathanson <alex@alexnathanson.com>
2020-08-03 19:12:29 -0400

    test

---
+++ clientPostIP.py
@@ -0,0 +1,20 @@
+import requests
+import time
+
+dstIP = '192.168.1.180'
+url = 'https://'+dstIP+'/postIP.php'
+
+
+headers = {
+    #'X-Auth-Key': KEY,
+    'Content-Type': 'application/x-www-form-urlencoded',
+}
+
+myIP = 	requests.get('http://whatismyip.akamai.com/').text
+
+#myobj = {'Stamp': time.time(), 'IP': myIP}
+myString = "api_key=tPmAT5Ab3j7F9&Stamp="+str(time.time())+"&IP="+myIP
+
+x = requests.post(url, data = myString)
+
+print(x.text)
---
+++ displayDB.php
@@ -0,0 +1,81 @@
+<!--
+THIS PAGE WILL BE FOR DISPLAYING PV DATA?
+-->
+
+
+<!DOCTYPE html>
+<html><body>
+<?php
+/*
+  Rui Santos
+  Complete project details at https://RandomNerdTutorials.com/esp32-esp8266-mysql-database-php/
+  
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files.
+  
+  The above copyright notice and this permission notice shall be included in all
+  copies or substantial portions of the Software.
+*/
+
+$servername = "localhost";
+
+// REPLACE with your Database name
+$dbname = "REPLACE_WITH_YOUR_DATABASE_NAME";
+// REPLACE with Database user
+$username = "REPLACE_WITH_YOUR_USERNAME";
+// REPLACE with Database user password
+$password = "REPLACE_WITH_YOUR_PASSWORD";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
+$sql = "SELECT id, sensor, location, value1, value2, value3, reading_time FROM SensorData ORDER BY id DESC";
+
+echo '<table cellspacing="5" cellpadding="5">
+      <tr> 
+        <td>ID</td> 
+        <td>Sensor</td> 
+        <td>Location</td> 
+        <td>Value 1</td> 
+        <td>Value 2</td>
+        <td>Value 3</td> 
+        <td>Timestamp</td> 
+      </tr>';
+ 
+if ($result = $conn->query($sql)) {
+    while ($row = $result->fetch_assoc()) {
+        $row_id = $row["id"];
+        $row_sensor = $row["sensor"];
+        $row_location = $row["location"];
+        $row_value1 = $row["value1"];
+        $row_value2 = $row["value2"]; 
+        $row_value3 = $row["value3"]; 
+        $row_reading_time = $row["reading_time"];
+        // Uncomment to set timezone to - 1 hour (you can change 1 to any number)
+        //$row_reading_time = date("Y-m-d H:i:s", strtotime("$row_reading_time - 1 hours"));
+      
+        // Uncomment to set timezone to + 4 hours (you can change 4 to any number)
+        //$row_reading_time = date("Y-m-d H:i:s", strtotime("$row_reading_time + 4 hours"));
+      
+        echo '<tr> 
+                <td>' . $row_id . '</td> 
+                <td>' . $row_sensor . '</td> 
+                <td>' . $row_location . '</td> 
+                <td>' . $row_value1 . '</td> 
+                <td>' . $row_value2 . '</td>
+                <td>' . $row_value3 . '</td> 
+                <td>' . $row_reading_time . '</td> 
+              </tr>';
+    }
+    $result->free();
+}
+
+$conn->close();
+?> 
+</table>
+</body>
+</html>

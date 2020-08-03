fb50d34ae7f8739bcbf7ae8f4e19e02813674d4c
alexnathanson <alex@alexnathanson.com>
2020-08-03 19:16:53 -0400

    php

--- displayDB.php
+++ displayDB.php
@@ -20,11 +20,11 @@ THIS PAGE WILL BE FOR DISPLAYING PV DATA?
 $servername = "localhost";
 
 // REPLACE with your Database name
-$dbname = "REPLACE_WITH_YOUR_DATABASE_NAME";
+$dbname = "ipDB";
 // REPLACE with Database user
-$username = "REPLACE_WITH_YOUR_USERNAME";
+$username = "root";
 // REPLACE with Database user password
-$password = "REPLACE_WITH_YOUR_PASSWORD";
+$password = "sp252vb";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
@@ -33,28 +33,19 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
 
-$sql = "SELECT id, sensor, location, value1, value2, value3, reading_time FROM SensorData ORDER BY id DESC";
+$sql = "SELECT Stamp, IP FROM ipList ORDER BY Stamp DESC";
 
 echo '<table cellspacing="5" cellpadding="5">
       <tr> 
-        <td>ID</td> 
-        <td>Sensor</td> 
-        <td>Location</td> 
-        <td>Value 1</td> 
-        <td>Value 2</td>
-        <td>Value 3</td> 
-        <td>Timestamp</td> 
+        <td>Time Stamp</td> 
+        <td>IP</td>  
       </tr>';
  
 if ($result = $conn->query($sql)) {
     while ($row = $result->fetch_assoc()) {
-        $row_id = $row["id"];
-        $row_sensor = $row["sensor"];
-        $row_location = $row["location"];
-        $row_value1 = $row["value1"];
-        $row_value2 = $row["value2"]; 
-        $row_value3 = $row["value3"]; 
-        $row_reading_time = $row["reading_time"];
+        $row_Stamp = $row["Stamp"];
+        $row_IP = $row["IP"];
+      
         // Uncomment to set timezone to - 1 hour (you can change 1 to any number)
         //$row_reading_time = date("Y-m-d H:i:s", strtotime("$row_reading_time - 1 hours"));
       
@@ -62,13 +53,8 @@ if ($result = $conn->query($sql)) {
         //$row_reading_time = date("Y-m-d H:i:s", strtotime("$row_reading_time + 4 hours"));
       
         echo '<tr> 
-                <td>' . $row_id . '</td> 
-                <td>' . $row_sensor . '</td> 
-                <td>' . $row_location . '</td> 
-                <td>' . $row_value1 . '</td> 
-                <td>' . $row_value2 . '</td>
-                <td>' . $row_value3 . '</td> 
-                <td>' . $row_reading_time . '</td> 
+                <td>' . $row_Stamp . '</td> 
+                <td>' . $row_IP . '</td> 
               </tr>';
     }
     $result->free();

9dfc8b5ff773c0716afa358deee5a7f3bf73ffc2
alexnathanson <alex@alexnathanson.com>
2020-08-25 01:02:47 -0400

    index

--- frontend/index.php
+++ frontend/index.php
@@ -1,62 +1,268 @@
 <!DOCTYPE html>
 <html>
+
 <head>
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.1.4/Chart.min.js"></script>
-	<script type="text/javascript" src="js/json-graph.js?=1"></script>
-	<title>Charge Controller Graph</title>
+
+<script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+
+<title>Solar Server</title>
+
+<h1>Solar Server!</h1>
 </head>
+
 <body>
 
-<?php 
-// get files in directory
-// https://www.php.net/manual/en/function.scandir.php
-$directory = getcwd() . '/data';
-$files = array_diff(scandir($directory), array('..', '.'));
+<?php
 
-// pass file to javascript (json-graph.js)
-// if csv, convert it to json
-foreach($files as $file){
-	if (strpos($file, 'csv') !== false) {
-		$json_from_csv = csvtojson($directory . "/" . $file, ",");
+//get the query string
+$date = htmlspecialchars($_GET["date"]);
 
-		// to read datatime correctly, replace space to T
-		// e.g., "2020-05-15 14:00:30.089197" becomes "2020-05-15T14:00:30.089197" 
-		$json_from_csv = preg_replace('/[[:space:]]+/', 'T', $json_from_csv);
-		echo "<a id='$file' href='#$file' onclick=getcsv('$json_from_csv'); return false;>$file</a><br>";
-	} else {
-		// https://stackoverflow.com/questions/9702040/how-pass-reference-to-this-on-href-javascript-function
-		echo "<a id='$file' href='#$file' onClick=getjson('$file'); return false;>$file</a><br>";
-	}
+$fileDate = date("Y-m-d");
+if ($date == 'yesterday'){
+	$fileDate = date("Y-m-") . (date(d)-1);
+}elseif( $date == 'before'){
+	$fileDate = date("Y-m-") . (date(d)-2);
 }
 
+//variables
+$fileName = "/home/pi/EPSolar_Tracer/data/tracerData" . $fileDate . ".csv";
+$rawDataArray = [];
 
-// convert csv to json
-// https://stackoverflow.com/questions/28118101/convert-csv-to-json-using-php
-function csvtojson($file,$delimiter) {
-    if (($handle = fopen($file, "r")) === false) {
-        die("can't open the file.");
-    }
+echo "<h3>Today's Date:</h3>" . $fileDate . "<br>";
 
-    $csv_headers = fgetcsv($handle, 4000, $delimiter);
-    $csv_json = array();
+echo "<p>View: <a href='/?date=today' id='today'>Today</a> | <a href='/?date=yesterday' id='yesterday'>Yesterday</a> | <a href='/?date=before' id='daybefore'>The Day Before</a></p>";
 
-    while ($row = fgetcsv($handle, 4000, $delimiter)) {
-        $csv_json[] = array_combine($csv_headers, $row);
-    }
+echo "<h3>File Name:</h3>". $fileName . "<br>";
 
-    fclose($handle);
-    return json_encode($csv_json);
+// current directory
+//echo getcwd() . "\n";
+
+echo "<h3>PV Data:</h3>";
+
+// Open the file for reading (from https://phpenthusiast.com/blog/parse-csv-with-php)
+if (($h = fopen("{$fileName}", "r")) !== FALSE) 
+{
+  // Each line in the file is converted into an individual array that we call $data
+  // The items of the array are comma separated
+  while (($data = fgetcsv($h, 1000, ",")) !== FALSE) 
+  {
+    // Each individual array is being pushed into the nested array
+    $rawDataArray[] = $data;		
+  }
+
+  // Close the file
+  fclose($h);
 }
+
+/*
+// Display the code in a readable format
+echo "<pre>";
+var_dump($rawDataArray);
+echo "</pre>";
+*/
+
+echo "<h4>Most Recent Data:</h4>";
+$buildNow = '<table border=1px>';
+//foreach($rawDataArray as $row)
+
+//header
+{
+$buildNow .= '<tr>';
+foreach($rawDataArray[0] as $item)  
+{
+$buildNow .= "<td>{$item}</td>";
+}
+$buildNow .= '</tr>';
+}
+
+//most recent data
+$dataCount = count($rawDataArray);
+$nowRow = $rawDataArray[$dataCount-1];
+
+{
+$buildNow .= '<tr>';
+foreach($nowRow as $item)
+{
+$buildNow .= "<td>{$item}</td>";
+}
+$buildNow .= '</tr>';
+}
+$buildNow .= '</table>';
+echo $buildNow;
+
+
+echo "<h4>Today:</h4>";
+
 
 ?>
 
-<canvas id="voltageChart"></canvas>
-<canvas id="currentChart"></canvas>
-<canvas id="powerChart"></canvas>
-<canvas id="batteryPercentage"></canvas>
-<table id="dataTable" border=1 style="text-align: center;"></table>
+<script type="text/javascript">
+	google.charts.load('current', {'packages':['corechart', 'line']});
+	google.charts.setOnLoadCallback(drawChart);
+
+	var phpData = <?php echo json_encode($rawDataArray) ?>;
+
+	//remove the time column from the end
+	for (var p =0; p <phpData.length; p++){
+		phpData[p].pop();
+	}
+      
+	Array.prototype.clone = function() {
+		return JSON.parse(JSON.stringify(this)); //deep copy
+
+		//return this.slice(0); //shallow copy
+	};
+//PV DATA
+	//var pvData = ;
+/*
+	for (var sp = 0; sp < pvData.length;sp++){
+		pvData[sp].splice(10,3);//remove columns 10-13
+		pvData[sp].splice(4,5);//remove colums 4-8
+	}
+	
+*/
+	var pvData = cleanData(phpData.clone(), "date");
+
+	//remember this happens AFTER the columns are shuffled around!
+	for (var sp = 0; sp < pvData.length;sp++){
+		pvData[sp].splice(1,3);//remove columns
+		pvData[sp].splice(5);//remove colums
+	}
+	
+	//console.log(pvData);
+
+//BAT DATA
+	//var batData = phpData.slice(4,6);
+
+	var batData = cleanData(phpData.clone(), "date");
+
+	//console.log(batData);
+	//remember this happens AFTER the columns are shuffled around!
+	for (var sp = 0; sp < batData.length;sp++){
+		batData[sp].splice(1,7);//remove columns
+		//batData[sp].splice(8);//remove colums
+	}
+	
+	//scale bat percentage to 0-100
+	for (var bper = 1; bper < batData.length; bper++){
+		batData[bper][1] *= 100.0;
+	}
+
+//LOAD DATA
+	//var loadData = phpData.slice(9);//if only 1 element it goes to the end
+	var loadData = cleanData(phpData.clone(), "date");
+
+	//remember this happens AFTER the columns are shuffled around!
+	for (var sp = 0; sp < loadData.length;sp++){
+		loadData[sp].splice(4);//remove columns
+		//batData[sp].splice(8);//remove colums
+	}
+	//select columns
+      //this only works for taking a contiguous subset
+      //var mapData = phpData.map(function(val){
+	  //  return val.slice(10,(val.length));
+		//});
+
+      function cleanData(tempData, stringForX){
+
+      	console.log(tempData[0]) //print headers
+
+      	//Set X axis
+      	var useAsX = 0;
+      	//find string in first row
+      	for (var getX = 0; getX < tempData[0].length; getX++){
+      		if (tempData[0][getX] == stringForX){
+      			useAsX = getX;
+      			console.log(stringForX + " " + useAsX);
+      			break;
+      		}
+      	}
+		//go through each row
+      	for (var i = 0; i < tempData.length; i++) {
+      	//send the first column to the back until the selected column is first
+	      	for (var c=0; c < useAsX;c++){
+			 	tempData[i][tempData[i].length-1] = tempData[i].shift();
+	      		}
+		}
+
+      //make floats, exclude header and X-axis
+      for (var i = 1; i < tempData.length; i++) {
+      	for (var c=1; c < tempData[i].length;c++){
+		 	tempData[i][c] = parseFloat(tempData[i][c]);
+      		}
+		}
+
+      //console.log(tempData);
+
+      return tempData;
+      }
+
+      function drawChart() {
+        var PVdataMap = google.visualization.arrayToDataTable(pvData);
+        var BATdataMap = google.visualization.arrayToDataTable(batData);
+        var LOADdataMap = google.visualization.arrayToDataTable(loadData);
+
+        //console.log(data);
+
+        var PVoptions = {
+          title: 'PV',
+          curveType: 'function',
+          legend: { position: 'bottom' },
+          width: 1500,
+        	height: 500
+        };
+
+        var BAToptions = {
+          title: 'Battery',
+          curveType: 'function',
+          legend: { position: 'bottom' },
+          width: 1500,
+        	height: 500
+        };
+
+        var LOADoptions = {
+          title: 'Load',
+          curveType: 'function',
+          legend: { position: 'bottom' },
+          width: 1500,
+        	height: 500
+        };
+
+        var PVchart = new google.visualization.LineChart(document.getElementById('PV_chart'));
+
+        PVchart.draw(PVdataMap, PVoptions);
+
+        var BATchart = new google.visualization.LineChart(document.getElementById('BAT_chart'));
+
+        BATchart.draw(BATdataMap, BAToptions);
+
+        var LOADchart = new google.visualization.LineChart(document.getElementById('LOAD_chart'));
+
+        LOADchart.draw(LOADdataMap, LOADoptions);
+    }
+
+    
+</script>
+
+<div id="PV_chart" style="width: 1500px; height: 500px"></div>
+<div id="BAT_chart" style="width: 1500px; height: 500px"></div>
+<div id="LOAD_chart" style="width: 1500px; height: 500px"></div>
+
+<?php
+//also from https://phpenthusiast.com/blog/parse-csv-with-php
+$build = '<table border=1px>';
+foreach($rawDataArray as $row)
+{
+$build .= '<tr>';
+foreach($row as $item)
+{
+$build .= "<td>{$item}</td>";
+}
+$build .= '</tr>';
+}
+$build .= '</table>';
+echo $build;
+?>
 
 </body>
 </html>
-
-
--- installation.md
+++ installation.md
@@ -1,4 +1,9 @@
 # Installation
+## Wiring
+This works with a USB to RS485 converter (ch340T chip model).
+* RJ45 blue => b
+* RJ45 green => a
+
 ## OS
 Configure device `sudo raspi-config` https://www.raspberrypi.org/documentation/configuration/raspi-config.md <br>
 * change password, connect to wifi, enable SSH, keyboard layout, set timezone, and any other necessry configurations.<br>
@@ -12,8 +17,26 @@ Configure device `sudo raspi-config` https://www.raspberrypi.org/documentation/c
 ## Python 3 Packages
 Install pip `sudo apt-get install python3-pip`<br>
 Install pymodbus `sudo pip3 install pymodbus`<br>
-Install pandas `sudo pip3 install pandas` (this should be refactored to not used pandas)
-Install numpy 'sudo pip3 uninstall numpy' followed by `sudo apt-get install python3-numpy `
+Install pandas `sudo pip3 install pandas` (this should be refactored to not used pandas)<br>
+Install numpy 'sudo pip3 uninstall numpy' followed by `sudo apt-get install python3-numpy`
+
 ## Server
 Install Apache `sudo apt-get install apache2 -y` (https://projects.raspberrypi.org/en/projects/lamp-web-server-with-wordpress/2)<br>
-Install PHP `sudo apt-get install php -y` (https://projects.raspberrypi.org/en/projects/lamp-web-server-with-wordpress/3)
+Install PHP `sudo apt-get install php -y` (https://projects.raspberrypi.org/en/projects/lamp-web-server-with-wordpress/3)<br>
+Change Apache default directory to the frontend directory (src: https://julienrenaux.fr/2015/04/06/changing-apache2-document-root-in-ubuntu-14-x/)
+* `cd /etc/apache2/sites-available`
+* `sudo nano 000-default.conf`
+* change `DocumentRoot /var/www/` to `DocumentRoot /home/pi/solar-protocol/frontend`
+* `sudo nano /etc/apache2/apache2.conf`
+* add this to the file
+`<Directory /home/pi/solar-protocol/frontend/>
+       Options Indexes FollowSymLinks
+       AllowOverride None
+       Require all granted
+</Directory>`
+* `sudo service apache2 restart`
+
+## Automate
+
+## Troubleshooting
+Run test.py to test connection between Pi and charge controller

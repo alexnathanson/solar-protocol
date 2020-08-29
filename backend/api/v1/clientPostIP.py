d8fc6ed710d4f76d03b0fa8055785d9b67afc5aa
alexnathanson <alex@alexnathanson.com>
2020-08-28 21:40:26 -0400

    post exceptions

--- backend/api/v1/clientPostIP.py
+++ backend/api/v1/clientPostIP.py
@@ -60,15 +60,15 @@ def makePosts(ipList):
 		try:
 			x = requests.post('http://'+dst+'/api/v1/api.php', headers=headers,data = myString)
 			print(x.text)
-			requests.raise_for_status()
+			#requests.raise_for_status()
 		except requests.exceptions.HTTPError as errh:
-		 	return "An Http Error occurred:" + repr(errh)
+		 	print("An Http Error occurred:" + repr(errh))
 		except requests.exceptions.ConnectionError as errc:
-			return "An Error Connecting to the API occurred:" + repr(errc)
+			print("An Error Connecting to the API occurred:" + repr(errc))
 		except requests.exceptions.Timeout as errt:
-		 	return "A Timeout Error occurred:" + repr(errt)
+		 	print("A Timeout Error occurred:" + repr(errt))
 		except requests.exceptions.RequestException as err:
-		 	return "An Unknown Error occurred" + repr(err)
+		 	print("An Unknown Error occurred" + repr(err))
 
 #wlan0 might need to be changed to eth0 if using an ethernet cable
 myMAC = getmac("wlan0")

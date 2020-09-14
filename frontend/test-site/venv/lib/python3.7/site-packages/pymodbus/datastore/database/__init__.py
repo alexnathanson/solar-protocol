a3fa1bced65040831a6792b5a039c8b0d228ff12
tegacodes <tega.brain@gmail.com>
2020-09-14 13:44:02 -0400

    updated text

--- frontend/test-site/venv/lib/python3.7/site-packages/pymodbus/datastore/database/__init__.py
+++
@@ -1,7 +0,0 @@
-from pymodbus.datastore.database.sql_datastore import SqlSlaveContext
-from pymodbus.datastore.database.redis_datastore import RedisSlaveContext
-
-#---------------------------------------------------------------------------#
-# Exported symbols
-#---------------------------------------------------------------------------#
-__all__ = ["SqlSlaveContext", "RedisSlaveContext"]
--- frontend/test-site/venv/lib/python3.7/site-packages/pymodbus/repl/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-Pymodbus REPL Module.
-
-Copyright (c) 2018 Riptide IO, Inc. All Rights Reserved.
-
-"""
-from __future__ import absolute_import, unicode_literals

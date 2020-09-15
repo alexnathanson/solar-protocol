bbadcf2cf7eaf08ab9e14c4a99b78fbb4f995c83
alexnathanson <alex@alexnathanson.com>
2020-09-15 13:11:34 -0400

    full file paths

--- backend/createHTML/create_html.py
+++ backend/createHTML/create_html.py
@@ -68,9 +68,9 @@ def read_csv():
 
 def check_energy(_data):
     if (_data["battery percentage"]) < 0.65:
-        template_file = open("templates/index-small.html").read()
+        template_file = open("/home/pi/solar-protocol/backend/createHTML/templates/index-small.html").read()
     else:
-        template_file = open("templates/index-large.html").read()
+        template_file = open("/home/pi/solar-protocol/backend/createHTML/templates/index-large.html").read()
 
     template = Template(template_file)
     rendered_html = template.render(

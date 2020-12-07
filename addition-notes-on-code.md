53766fe5e11d9ed514c0644e7cfba04101f61e17
alexnathanson <alex@alexnathanson.com>
2020-12-06 20:03:51 -0500

    admin password tests

---
+++ addition-notes-on-code.md
@@ -0,0 +1,6 @@
+# Additional notes on the code
+
+
+## Admin console
+
+The password for accessing the admin console is not intended to be super secure for the time being. It is really just meant to keep casual users out.
--- frontend/admin/test.php
+++ frontend/admin/test.php
@@ -1,34 +1,55 @@
-<?php
-$username = "the_username_here";
-$password = "the_password_here";
-$nonsense = "supercalifragilisticexpialidocious";
 
-if (isset($_COOKIE['PrivatePageLogin'])) {
-   if ($_COOKIE['PrivatePageLogin'] == md5($password.$nonsense)) {
-?>
+<!DOCTYPE html>
+<html>
 
-    <!-- LOGGED IN CONTENT HERE -->
+<head>
 
-<?php
-      exit;
-   } else {
-      echo "Bad Cookie.";
-      exit;
+<script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+
+<title>Solar Server</title>
+
+</head>
+
+<body>
+   <?php
+   $username = "the_username_here";
+   $password = "the_password_here";
+   $nonsense = "supercalifragilisticexpialidocious";
+
+   if (isset($_COOKIE['PrivatePageLogin'])) {
+      if ($_COOKIE['PrivatePageLogin'] == md5($password.$nonsense)) {
+   ?>
+
+       <!-- LOGGED IN CONTENT HERE -->
+
+   <?php
+         exit;
+      } else {
+         echo "Bad Cookie.";
+         exit;
+      }
    }
-}
 
-if (isset($_GET['p']) && $_GET['p'] == "login") {
-   if ($_POST['user'] != $username) {
-      echo "Sorry, that username does not match.";
-      exit;
-   } else if ($_POST['keypass'] != $password) {
-      echo "Sorry, that password does not match.";
-      exit;
-   } else if ($_POST['user'] == $username && $_POST['keypass'] == $password) {
-      setcookie('PrivatePageLogin', md5($_POST['keypass'].$nonsense));
-      header("Location: $_SERVER[PHP_SELF]");
-   } else {
-      echo "Sorry, you could not be logged in at this time.";
+   if (isset($_GET['p']) && $_GET['p'] == "login") {
+      if ($_POST['user'] != $username) {
+         echo "Sorry, that username does not match.";
+         exit;
+      } else if ($_POST['keypass'] != $password) {
+         echo "Sorry, that password does not match.";
+         exit;
+      } else if ($_POST['user'] == $username && $_POST['keypass'] == $password) {
+         setcookie('PrivatePageLogin', md5($_POST['keypass'].$nonsense));
+         header("Location: $_SERVER[PHP_SELF]");
+      } else {
+         echo "Sorry, you could not be logged in at this time.";
+      }
    }
-}
-?>
+   ?>
+
+   <form action="<?php echo $_SERVER['PHP_SELF']; ?>?p=login" method="post">
+   <label><input type="text" name="user" id="user" /> Name</label><br />
+   <label><input type="password" name="keypass" id="keypass" /> Password</label><br />
+   <input type="submit" id="submit" value="Login" />
+   </form>
+
+</body>

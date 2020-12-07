945a22bf638315c5da7c743489dd81356ffba186
alexnathanson <alex@alexnathanson.com>
2020-12-06 23:24:27 -0500

    hash generator

--- frontend/api/v1-1/hash.php
+++ frontend/api/v1-1/hash.php
@@ -18,10 +18,7 @@ if( isset($_POST['hash'])) {
 <body>
 
 <form method="POST">
-  <?php if( $_SERVER['REQUEST_METHOD'] == 'POST' ) { ?>
-    Invalid password
-  <?php } ?>
-  <p>Enter password for access:</p>
+  <p>Enter password to hash:</p>
   <input type="hash" name="hash">
   <button type="submit">Submit</button>
 </form>

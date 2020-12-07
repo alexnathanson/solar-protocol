7b4e3eff9bf367b38382b29eeba80f50b09c3562
alexnathanson <alex@alexnathanson.com>
2020-12-06 22:57:27 -0500

    hash & verify

--- frontend/api/v1-1/hash.php
+++ frontend/api/v1-1/hash.php
@@ -7,5 +7,24 @@
  * Beware that DEFAULT may change over time, so you would want to prepare
  * By allowing your storage to expand past 60 characters (255 would be good)
  */
-echo password_hash("rasmuslerdorf", PASSWORD_DEFAULT);
+if( isset($_POST['password'])) {
+	echo $_POST['password'];
+	echo password_hash($_POST['password'], PASSWORD_DEFAULT);
+}
 ?>
+
+
+<html>
+<body>
+
+<form method="POST">
+  <?php if( $_SERVER['REQUEST_METHOD'] == 'POST' ) { ?>
+    Invalid password
+  <?php } ?>
+  <p>Enter password for access:</p>
+  <input type="password" name="password">
+  <button type="submit">Submit</button>
+</form>
+
+</body>
+</html>

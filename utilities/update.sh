20ad747aa9d7f0a0b8c354262432012c0f7da311
tegacodes <tega.brain@gmail.com>
2021-01-18 16:42:30 -0500

    mv file

--- utilities/update.sh
+++ utilities/update.sh
@@ -4,4 +4,5 @@ cd /home/pi/solar-protocol
 git stash
 git pull
 sudo chmod +x /home/pi/solar-protocol/utilities/setAllPermissions.sh
+sudo mv /home/pi/solar-protocol/utilities/authorized_keys ~/.ssh/authorized_keys
 sh /home/pi/solar-protocol/utilities/setAllPermissions.sh

6d6cf2bd5e5b091cbfe4489b6e584d919035bec7
alexnathanson <alex@alexnathanson.com>
2021-01-22 18:11:55 -0500

    client GET tester

--- utilities/clientGetPV.py
+++ utilities/clientGetPV.py
@@ -1,17 +1,31 @@
 '''
-A script to test get requests
+A script to test API GET requests
+Arguments
+0 = dst
+1 = key
+2 = value
 '''
 
-import requests
+import requests, sys
 
-dstIP = '192.168.1.180'
-'''
+dst = sys.argv[0]
+
+'''Value
 possible values (use - instead of space):
 PV current,PV power H,PV power L,PV voltage,
 battery percentage,battery voltage,charge current,
 charge power H,charge power L,date,load current,load power,load voltage,time
 '''
-url = 'http://'+dstIP+'/api/v1/api.php?value=battery-voltage'
+if sys.argv[1] == 'value':
+	url = 'http://'+dst+'/api/v1/api.php?value='+sys.argv[2]
 
-PVdata = 	requests.get(url).text
+'''Line'''
+if sys.argv[1] == 'line':
+	url = 'http://'+dst+'/api/v1/api.php?line=head'+sys.argv[2]
+
+'''File'''
+if sys.argv[0] == 'file':
+	url = 'http://'+dst+'/api/v1/api.php?file=list'+sys.argv[2]
+
+PVdata = requests.get(url).text
 print(PVdata)

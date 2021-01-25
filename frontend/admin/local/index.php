83ee6a5adba4743b5072dcc397c59b0e60147dd5
alexnathanson <alex@alexnathanson.com>
2021-01-25 15:45:07 -0500

    local form

--- frontend/admin/local/index.php
+++ frontend/admin/local/index.php
@@ -5,11 +5,46 @@
   <?php if( $_SERVER['REQUEST_METHOD'] == 'POST' ) { ?>
     Invalid password
   <?php } ?>
-  <p>Server Location</p>
-  <input type="Location" name="location">
-  p>Server Details</p>
-  <input type="details" name="details">
-  <button type="submit">Submit</button>
+  
+  <p>Name</p>
+  <input type="text" name="name">
+  
+  <p>Description</p>
+  <input type="text" name="description">
+  
+   <p>Location</p>
+  <input type="text" name="location">
+
+   <p>City</p>
+  <input type="text" name="city">
+
+   <p>Country</p>
+  <input type="text" name="country">
+
+ 	<p>Latitude</p>
+  <input type="text" name="lat">
+
+   <p>longitude</p>
+  <input type="text" name="long">
+
+ <p>API key</p>
+  <input type="password" name="apiKey">
+
+   <p>Background Color</p>
+  <input type="text" name="bgColor">
+
+   <p>Server Color</p>
+  <input type="text" name="serverColor">
+
+  <!--make a drop down menu-->
+   <p>Font</p>
+  <input type="text" name="font">
+
+   <p>Border Style</p>
+  <input type="text" name="borderStyle">
+
+  <p></p>
+  <button type="submit">Update</button>
 </form>
 
 </body>

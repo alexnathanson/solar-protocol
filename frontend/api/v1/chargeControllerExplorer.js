6b787d4df4914a181d869bfb04178d4abb5807d5
alexnathanson <alex@alexnathanson.com>
2021-01-26 12:35:26 -0500

    base url reg ex

--- frontend/api/v1/chargeControllerExplorer.js
+++ frontend/api/v1/chargeControllerExplorer.js
@@ -1,9 +1,15 @@
 const params = new URLSearchParams(window.location.search);
 
 //example http://solarprotocol.net/api/v1/chargecontroller.php?file=list
-let result = JSON.parse(httpGet('108.29.41.133.net/api/v1/chargecontroller.php?' + params.get("key") + "=" + params.get("value")));
+let result = JSON.parse(httpGet(getBaseUrl() + '/api/v1/chargecontroller.php?' + params.get("key") + "=" + params.get("value")));
 
 console.log(result);
+
+function getBaseUrl() {
+    var re = new RegExp(/^.*\/\/[^\/]+/);
+    console.log(re.exec(window.location.href));
+    return re.exec(window.location.href);
+}
 
 function httpGet(theUrl)
 {

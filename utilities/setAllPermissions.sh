3888a5a4a50c68ff49160440020a81ce215ca839
alexnathanson <alex@alexnathanson.com>
2021-01-27 17:54:28 -0500

    set local permissions

--- utilities/setAllPermissions.sh
+++ utilities/setAllPermissions.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+sudo chmod a+w /home/pi/local/localW.json
 sudo chmod a+w /home/pi/solar-protocol/backend/api/v1/deviceList.json
 sudo chmod +x /home/pi/solar-protocol/backend/update_ip2.sh
 sudo chmod +x /home/pi/solar-protocol/charge-controller/csv_datalogger.py

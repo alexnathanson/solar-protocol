19784ec4447fb168cd4076c2700d27035aeefd4b
alexnathanson <alex@alexnathanson.com>
2021-01-31 01:20:56 -0500

    check for params

--- frontend/api/v1/chargeControllerExplorer.js
+++ frontend/api/v1/chargeControllerExplorer.js
@@ -1,46 +1,50 @@
 const params = new URLSearchParams(window.location.search);
 
-//this could be much simplied by getting the url search params in a string and just adding it to the url...
+if(Object.keys(params).length != 0){
 
-let endPt,endPtVal,mod,modVal = ""; 
-let modified = false;
-if(params.get("value")){
-	endPt = "value";
-	endPtVal = params.get("value");
+	//this could be much simplied by getting the url search params in a string and just adding it to the url...
 
-	if(params.get("duration")){
-		mod= "duration";
-		modVal=params.get("duration");
-		modified = true;
+	let endPt,endPtVal,mod,modVal = ""; 
+	let modified = false;
+
+	if(params.get("value")){
+		endPt = "value";
+		endPtVal = params.get("value");
+
+		if(params.get("duration")){
+			mod= "duration";
+			modVal=params.get("duration");
+			modified = true;
+		}
+	} else if(params.get("line")){
+		endPt = "line";
+		endPtVal = params.get("line");
+	} else if(params.get("day")){
+		endPt = "day";
+		endPtVal = params.get("day");
+	} else if (params.get("systemInfo")){
+		endPt = "systemInfo";
+		endPtVal = params.get("systemInfo");
 	}
-} else if(params.get("line")){
-	endPt = "line";
-	endPtVal = params.get("line");
-} else if(params.get("day")){
-	endPt = "day";
-	endPtVal = params.get("day");
-} else if (params.get("systemInfo")){
-	endPt = "systemInfo";
-	endPtVal = params.get("systemInfo");
+
+	//example http://solarprotocol.net/api/v1/chargecontroller.php?file=list
+
+	let dst = getBaseUrl() + '/api/v1/chargecontroller.php?' + endPt + "=" + endPtVal;
+
+	if(modified){
+		dst = dst + "&" + mod + "=" + modVal;
+	}
+
+	console.log(dst);
+
+	let result = httpGet(dst);
+
+	if(typeof result === "object"){
+		result = JSON.parse(result);
+	}
+
+	console.log(result);
 }
-
-//example http://solarprotocol.net/api/v1/chargecontroller.php?file=list
-
-let dst = getBaseUrl() + '/api/v1/chargecontroller.php?' + endPt + "=" + endPtVal;
-
-if(modified){
-	dst = dst + "&" + mod + "=" + modVal;
-}
-
-console.log(dst);
-
-let result = httpGet(dst);
-
-if(typeof result === "object"){
-	result = JSON.parse(result);
-}
-
-console.log(result);
 
 function getBaseUrl() {
     var re = new RegExp(/^.*\/\/[^\/]+/);

2164b610ee38944225741d8fdc3a4abccf0cbe51
alexnathanson <alex@alexnathanson.com>
2021-02-07 12:22:01 -0500

    serve file

--- frontend/redirect/images.php
+++ frontend/redirect/images.php
@@ -1,6 +1,7 @@
 <?php
 
 $locDir = "/home/pi/local/";
+$filepath= "";
 
 if ($_SERVER["REQUEST_METHOD"] == "GET") {
 	//NOTE: nothing can be output prior to the image for this to work
@@ -12,26 +13,34 @@ if ($_SERVER["REQUEST_METHOD"] == "GET") {
 	if (file_exists($filepath)){
 		if(strpos($filepath, "jpg")){
 			header("Content-type: image/jpg");
+			serveImg();
 		} else if(strpos($filepath, "jpeg")){
 			header("Content-type: image/jpeg");
+			serveImg();
 		}  else if(strpos($filepath, "png")){
 			header("Content-type: image/png");
+			serveImg();
 		} else if(strpos($filepath, "gif")){
 			header("Content-type: image/gif");
+			serveImg();
 		} else {
 			notFound();
-			return;
+			//return;
 		}
-		header("Accept-Ranges: bytes");
-		header('Content-Length: ' . filesize($filepath));
-		//header("Last-Modified: Fri, 03 Mar 2004 06:32:31 GMT");
-		readfile($filepath);
 	} else {
 		notFound();
 	}
 	
 }
 
+function serveImg(){
+	global $filepath;
+
+	header("Accept-Ranges: bytes");
+	header('Content-Length: ' . filesize($filepath));
+	//header("Last-Modified: Fri, 03 Mar 2004 06:32:31 GMT");
+	readfile($filepath);
+}
 function notFound(){
 	header( "HTTP/1.0 404 Not Found");
 	header("Content-type: image/jpeg");

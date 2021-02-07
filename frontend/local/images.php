e0ee749a80e24a3553514dc628f0fde3c5079e08
alexnathanson <alex@alexnathanson.com>
2021-02-07 10:46:49 -0500

    image redirect

--- frontend/local/images.php
+++ frontend/local/images.php
@@ -14,23 +14,33 @@ $locDir = "/home/pi/local/";
 if ($_SERVER["REQUEST_METHOD"] == "GET") {
 	var_dump($_GET);
 
+
+	// get the file name
+	$filepath= $locDir . @$_GET['file'];
+	 
 	// basic headers
 	header("Content-type: image/png");
-	header("Expires: Mon, 1 Jan 2099 05:00:00 GMT");
+/*	header("Expires: Mon, 1 Jan 2099 05:00:00 GMT");
 	header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
 	header("Cache-Control: no-store, no-cache, must-revalidate");
 	header("Cache-Control: post-check=0, pre-check=0", false);
-	header("Pragma: no-cache");
-	 
-	// get the file name
-	$file= $locDir . @$_GET['file'];
-	 
+	header("Pragma: no-cache");*/
+
+
+	header("Accept-Ranges: bytes");
+	header('Content-Length: ' . filesize($filepath));
+	header("Last-Modified: Fri, 03 Mar 2004 06:32:31 GMT");
+	readfile($filepath);
+
+
+
+
 	// get the size for content length
-	$size= filesize($file);
-	header("Content-Length: $size bytes");
+/*	$size= filesize($file);
+	header("Content-Length: $size bytes");*/
 	 
 	// output the file contents
-	echo readfile($file);
+	echo readfile($filepath);
 	
 }
 

4c25ebf35a8844fbabcb8e255f7ecbbef5afa4db
alexnathanson <alex@alexnathanson.com>
2021-02-07 10:40:18 -0500

    redirect

--- frontend/local/images.php
+++ frontend/local/images.php
@@ -9,32 +9,30 @@
 	    }
 	})*/
 
-	$locDir = "/home/pi/local/";
+$locDir = "/home/pi/local/";
 
-	if ($_SERVER["REQUEST_METHOD"] == "GET") {
-		var_dump($_GET);
+if ($_SERVER["REQUEST_METHOD"] == "GET") {
+	var_dump($_GET);
 
-		// basic headers
-		header("Content-type: image/png");
-		header("Expires: Mon, 1 Jan 2099 05:00:00 GMT");
-		header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
-		header("Cache-Control: no-store, no-cache, must-revalidate");
-		header("Cache-Control: post-check=0, pre-check=0", false);
-		header("Pragma: no-cache");
-		 
-		// get the file name
-		$file= $locDir . @$_GET['file'];
-		 
-		// get the size for content length
-		$size= filesize($file);
-		header("Content-Length: $size bytes");
-		 
-		// output the file contents
-		readfile($file);
-		 
-
-		
-	}
+	// basic headers
+	header("Content-type: image/png");
+	header("Expires: Mon, 1 Jan 2099 05:00:00 GMT");
+	header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
+	header("Cache-Control: no-store, no-cache, must-revalidate");
+	header("Cache-Control: post-check=0, pre-check=0", false);
+	header("Pragma: no-cache");
+	 
+	// get the file name
+	$file= $locDir . @$_GET['file'];
+	 
+	// get the size for content length
+	$size= filesize($file);
+	header("Content-Length: $size bytes");
+	 
+	// output the file contents
+	echo readfile($file);
+	
+}
 
 
 

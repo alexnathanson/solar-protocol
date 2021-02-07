9ea7a25aa8e216784f73a1cde59039f3e71df2ea
alexnathanson <alex@alexnathanson.com>
2021-02-07 13:04:24 -0500

    permissions www

--- utilities/setAllPermissions.sh
+++ utilities/setAllPermissions.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 sudo chmod a+w /home/pi/local/local.json
-sudo chmod a+w /home/pi/local
+sudo chmod a+w /home/pi/local/www
 sudo chmod a+w /home/pi/solar-protocol/backend/api/v1/deviceList.json
 sudo chmod +x /home/pi/solar-protocol/backend/update_ip2.sh
 sudo chmod +x /home/pi/solar-protocol/charge-controller/csv_datalogger.py
 sudo chmod a+w /home/pi/solar-protocol/frontend/index.html
 sudo chmod +x /home/pi/solar-protocol/utilities/update.sh
 
-echo "Permissions set for local directory, local.json, deviceList.json, update_ip2.sh, csv_datalogger.py, and index.html"
+echo "Permissions set for local www directory, local.json, deviceList.json, update_ip2.sh, csv_datalogger.py, and index.html"

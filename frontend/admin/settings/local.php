36c6015564da6bc1f1a3923f2a121f7f78a14523
alexnathanson <alex@alexnathanson.com>
2021-02-07 21:25:36 -0500

    file path

--- frontend/admin/settings/local.php
+++ frontend/admin/settings/local.php
@@ -107,7 +107,7 @@ function getFile($fileName){
 <h1><a href="/">Solar Protocol</a> - Admin Console</h1>
 <span>Logged in as <?php echo $_SESSION["username"]?> <a href="?logout">(Logout)</a></span>
 
-<p><a href="/admin">Network Activity</a> | <a href="/admin/local.php">Local Data</a> | <a href="/admin/settings">Settings</a> | <a href="/admin/settings/local">Local Content</a></p>
+<p><a href="/admin">Network Activity</a> | <a href="/admin/local.php">Local Data</a> | <a href="/admin/settings">Settings</a> | <a href="/admin/settings/local.php">Local Content</a></p>
 
 <h2>Local Content</h2>
 

6003edf9f9e19d496acb610803d86b48174ed763
alexnathanson <alex@alexnathanson.com>
2021-02-07 22:26:40 -0500

    dumb mistake

--- frontend/admin/settings/local.php
+++ frontend/admin/settings/local.php
@@ -23,7 +23,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
   //make new directory
   if(isset($_POST['newDirectory']) && isset($_POST['parent'])){
     $newDir = $_POST['newDirectory'] . $_POST['parent'];
-    echo $newDir
+    echo $newDir;
     mkdir($newDir);
   }
 }

cb44dc1b659bb61145d5ce018d1314af89d62f88
alexnathanson <alex@alexnathanson.com>
2021-02-09 15:36:14 -0500

    text upload

--- frontend/admin/settings/upload.php
+++ frontend/admin/settings/upload.php
@@ -12,48 +12,34 @@ function uploadIt(){
     $uploadOk = 1;
 
 
-    if(strpos(mime_content_type($target_file),"image")!== false ){
+    if(strpos(mime_content_type($_FILES["fileToUpload"]["tmp_name"]),"image")!== false ){
       imageUpload($target_file);
-    } else if (strpos(mime_content_type($target_file),"text")!== false ){
+    } else if (strpos(mime_content_type($_FILES["fileToUpload"]["tmp_name"]),"text")!== false ){
       textUpload($target_file);
     }
-/*
-    $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
-
-    // Check if image file is a actual image or fake image
-    if(isset($_POST["submit"])) {
-      $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
-      if($check !== false) {
-        echo "File is an image - " . $check["mime"] . ".";
-        $uploadOk = 1;
-      } else {
-        echo "File is not an image.";
-        $uploadOk = 0;
-      }
-    }*/
 
     // Check if file already exists
     //this might need a condition to delete the old file...
     if (isset($_POST["replace"]) == 0 && file_exists($target_file)) {
-      echo "Sorry, file already exists.";
+      echo "<br>Sorry, file already exists.";
       $uploadOk = 0;
     }
 
     // Check file size
     if ($_FILES["fileToUpload"]["size"] > 500000) {
-      echo "Sorry, your file is too large.";
+      echo "<br>Sorry, your file is too large.";
       $uploadOk = 0;
     }
 
     // Check if $uploadOk is set to 0 by an error
     if ($uploadOk == 0) {
-      echo "Sorry, your file was not uploaded.";
+      echo "<br>Sorry, your file was not uploaded.";
     // if everything is ok, try to upload file
     } else {
       if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
         echo "The file ". htmlspecialchars( basename( $_FILES["fileToUpload"]["name"])). " has been uploaded.";
       } else {
-        echo "Sorry, there was an error uploading your file.";
+        echo "<br>Sorry, there was an error uploading your file.";
       }
     }
   }
@@ -88,13 +74,15 @@ function imageUpload($iF){
     }
 }
 
-function textUpload(){
+function textUpload($tF){
   //supported file types html, css
   // php is disable on server for this directory
   global $uploadOk;
 
+  $textFileType = strtolower(pathinfo($tF,PATHINFO_EXTENSION));
+
   // Allow certain file formats
-  if($imageFileType != "html" && $imageFileType != "css") {
+  if($textFileType != "html" && $textFileType != "css") {
     echo "Sorry, only HTML and CSS files are allowed.";
     $uploadOk = 0;
   }

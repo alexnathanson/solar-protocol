b7b601f39c29fb35b0c845b300092384f5538364
alexnathanson <alex@alexnathanson.com>
2021-02-10 14:14:27 -0500

    network

--- frontend/network/index.php
+++ frontend/network/index.php
@@ -1,18 +1,34 @@
+<?php
+
+	$deviceInfoFile = "/home/pi/solar-protocol/backend/api/v1/deviceList.json";
+	//Get device
+	$deviceInfo = json_decode(getFile($deviceInfoFile), true);
+
+	var_dump($deviceList);
+
+	//make list of link
+
+	
+function getFile($fileName){
+  //echo $fileName;
+  try{
+    return file_get_contents($fileName);
+  }
+  catch(Exception $e) {
+    echo $fileName;
+    return FALSE;
+  }
+}
+
+?>
+
+
 <!DOCTYPE html>
 <html>
 
 <head>
 </head>
 <body>
-	<?php
 
-		$deviceInfoFile = "/home/pi/solar-protocol/backend/api/v1/deviceList.json";
-		//Get device
-		$deviceInfo = json_decode(getFile($deviceInfoFile), true);
-
-		var_dump($deviceList);
-
-		//make list of link
-	?>
 </body>
 </html>

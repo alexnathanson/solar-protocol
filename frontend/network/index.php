f053e173b74adc8a10519b4f6d0793f6c36e497a
alexnathanson <alex@alexnathanson.com>
2021-02-10 14:38:25 -0500

    descriptions

--- frontend/network/index.php
+++ frontend/network/index.php
@@ -6,11 +6,17 @@
 
 	foreach ($deviceInfo as $key => $value) {
 
-		echo "<br>".file_get_contents('http://' . $value['ip'] . "/api/v1/api.php?value=PV-voltage");
+		#echo "<br>".file_get_contents('http://' . $value['ip'] . "/api/v1/api.php?value=PV-voltage");
+
+		//add a try section to check that the site is online
 
 		//add new link
-		echo "<br>" . $value['name']. ": <a href='http://". $value['ip'] . "/local' target='_blank'>".$value['ip']."</a>";
-
+		echo "<h3>" . $value['name'] . "</h3>";
+		echo "<p>";
+		echo "About this site: " .$value['description']; 
+		echo "<br><a href='http://". $value['ip'] . "/local' target='_blank'>".$value['ip']."</a>";
+		echo "</p>";
+		
 		//var_dump($value);
 	}
 

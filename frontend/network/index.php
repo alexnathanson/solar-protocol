50d5c1cd825a730ecea0a7cbbbe68e64db1e2488
alexnathanson <alex@alexnathanson.com>
2021-02-10 17:17:59 -0500

    syntax

--- frontend/network/index.php
+++ frontend/network/index.php
@@ -9,9 +9,9 @@
 		#echo "<br>".file_get_contents('http://' . $value['ip'] . "/api/v1/api.php?value=PV-voltage");
 
 		//add a try section to check that the site is online
-		
+
 		//add new link
-		echo "<div style='padding: 10px;border: 2px solid black;margin-top: 10px;margin-bottom: 10px;'>"
+		echo "<div style='padding: 10px;border: 2px solid black;margin-top: 10px;margin-bottom: 10px;'>";
 		echo "<h3>" . $value['name'] . "</h3>";
 		echo "About this site: " .$value['description']; 
 		echo "<br><a href='http://". $value['ip'] . "/local' target='_blank'>".$value['ip']."</a>";

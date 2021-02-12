b632c9235747069466ae9e3ebdac2f1a469471a7
alexnathanson <alex@alexnathanson.com>
2021-02-11 21:10:07 -0500

    timeout

--- frontend/redirect/network.php
+++ frontend/redirect/network.php
@@ -9,7 +9,7 @@ $listNetwork = true;
 
 $default_socket_timeout = ini_get('default_socket_timeout');
 //echo $default_socket_timeout;
-ini_set('default_socket_timeout', 2);
+ini_set('default_socket_timeout', 1);
 
 
 if ($_SERVER["REQUEST_METHOD"] == "GET") {

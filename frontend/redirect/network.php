7bcc2c1ccb39a1027c0517e4312bbe7f3285e3b5
alexnathanson <alex@alexnathanson.com>
2021-06-06 23:23:45 -0400

    strpos css

--- frontend/redirect/network.php
+++ frontend/redirect/network.php
@@ -38,7 +38,7 @@ if ($_SERVER["REQUEST_METHOD"] == "GET") {
 			if(isset($_GET['path']) && $_GET['path'] != ""){
 
 				//set mime time for css
-				if(strpos($_GET['path'], "css")){
+				if(strpos($_GET['path'], "css") !== false){
 					header("content-type: text/css");
 				} else {
 					//set mime type for images
@@ -79,112 +79,22 @@ if ($_SERVER["REQUEST_METHOD"] == "GET") {
 				$listNetwork = true;
 			}
 			
-			str_replace(
+			/*str_replace(
 			   "<body>", 
 			   "<body><div style='padding: 10px;border: 2px solid black;margin-top: 10px;margin-bottom: 10px;'><h1><a href='/''>Solar Protocol</a> - Network Sites</h1></div>",
 			    $redirected
-			);		
+			);		*/
 		}
 	}
 }
 
 //if the requested page doesn't exist, redirect to the main network page
 if($listNetwork == true){
-	//listNetworkSites();
 	header("Location: /network.html");
 	die();
 }
 
-//this was the old programatic way to create the network list pages - delete once updated via the templaters properly
-function listNetworkSites(){
-	global $deviceInfo;
-
-	$newString = "";
-	/*$newString .= "<!DOCTYPE html><html><head><title>Solar Server</title></head><body><div style='padding: 10px;border: 2px solid black;margin-top: 10px;margin-bottom: 10px;'><h1><a href='/''>Solar Protocol</a> - Network Sites</h1></div>";*/
-
-	foreach ($deviceInfo as $key => $value) {
-
-		//echo "<br>".file_get_contents('http://' . $value['ip'] . "/api/v1/api.php?value=PV-voltage");
-
-		//add a try section to check that the site is online
-
-		//add new link
-		$sC = file_get_contents('http://'.$value['ip'].'/api/v1/chargecontroller.php?systemInfo=color');
-
-		$newString .= "<div style='padding: 10px;border: 2px solid black;margin-top: 10px;margin-bottom: 10px;float: left;background-color:".$sC.";width:100%'><div style='width:48%;float: left;'>";
-
-		$newString .= "<h3>" . $value['name'] . "</h3>";
-
-		$newString .= "<p>Status:";
-		$status = true;
-		if(@checkStatus($value['ip'])){
-			$newString .= " online</p>";
-		} else {
-			$newString .= " offline</p>";
-			$status = false;
-		}
-
-		$newString .= "Last check-in: " . date('r', $value['time stamp']);
-		
-		
-		$newString .= "<p>About this site: " . file_get_contents('http://'.$value['ip']."/api/v1/chargecontroller.php?systemInfo=description") . "</p>"; 
-
-		if($status == true){
-			$newString .= "<p><a href='http://solarprotocol.net/network/". formatURL($value['name']) . "' target='_blank'>http://solarprotocol.net/network/".formatURL($value['name'])."</a></p>";
-		} else {
-			$newString .= "<p>http://solarprotocol.net/network/".formatURL($value['name'])."</p>";
-		}
-
-		//image
-		if($status == true){
-			$newString .= "</div><div style='width:48%;float:right;'><img src='http://". $value['ip'] . "/local/serverprofile.jpg'style='width: 100%; height: auto;'></div>";
-		} else {
-			$newString .= "</div><div style='width:48%;float:right;'></div>";
-		}
-
-		$newString .= "</div>";
-
-		//var_dump($value);
-	}
-
-	$networkTemplate = file_get_contents("/home/pi/solar-protocol/frontend/network.html");
-
-	$networkTemplate = str_replace(
-				   '<div class="network-overview">', 
-				   '<div class="network-overview">' . $newString,
-				    $networkTemplate
-				);
-
-	$networkTemplate = str_replace(
-				   'href="style-small.css"', 
-				   'href="/style-small.css"',
-				    $networkTemplate
-				);
-
-	$networkTemplate = str_replace(
-				   'href="style-large.css"', 
-				   'href="/style-large.css"',
-				    $networkTemplate
-				);
-	
-	//map image
-	/*$networkTemplate = str_replace(
-				   'src="images/network.gif"', 
-				   'src="/images/network.gif"',
-				    $networkTemplate
-				);*/
-
-	//images
-	/*$networkTemplate = str_replace(
-				   'src="', 
-				   'src="/',
-				    $networkTemplate
-				);*/
-
-	echo $networkTemplate;
-
-}
-
+//not being used - could probably be deleted
 function checkStatus($checkIP){
 	$s = false;
 	

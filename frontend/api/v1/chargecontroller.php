c4f0859bf6b708287414b6f283f2707ed8a3e34a
alexnathanson <alex@alexnathanson.com>
2021-06-11 20:05:16 -0400

    json decode

--- frontend/api/v1/chargecontroller.php
+++ frontend/api/v1/chargecontroller.php
@@ -186,7 +186,7 @@ if ($_SERVER["REQUEST_METHOD"] == "GET") {
         "location" => $infoArray["location"],
         "city" => $infoArray["city"],
         "country" => $infoArray["country"]);
-      echo $infoDump;
+      echo json_decode($infoDump);
     }
   }
 }

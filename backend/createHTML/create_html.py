6963bce405ac32713c45f2c74822d1ec3ccffacb
alexnathanson <alex@alexnathanson.com>
2021-06-11 20:38:43 -0400

    response json

--- backend/createHTML/create_html.py
+++ backend/createHTML/create_html.py
@@ -288,11 +288,14 @@ def get_pv_value(dst):
         print("An Unknown Error occurred" + repr(err))
 
 #return data from a particular server
-def getAPIData(apiEndPoint, dst):
+def getAPIData(apiEndPoint, dst, format):
     try:
         #returns a single value
         response = requests.get('http://' + dst + '/api/v1/'+apiEndPoint, timeout = 5)
-        return response.text
+        if format == json:
+            return response.json
+        else:
+            return response.text
     except requests.exceptions.HTTPError as errh:
         print("An Http Error occurred:" + repr(errh))
     except requests.exceptions.ConnectionError as errc:
@@ -406,7 +409,7 @@ def main():
     for key, value in deviceList_data.items():
         try:
             # item["ip"] = value #add IPs to server data
-            sInfo = getAPIData('chargecontroller.php?systemInfo=dump',value)
+            sInfo = getAPIData('chargecontroller.php?systemInfo=dump',value, 'json')
             print("#2")
             print(sInfo)
             print(type(sInfo))

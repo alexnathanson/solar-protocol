6178082d4330014282496eef2b5a3161c5556f75
alexnathanson <alex@alexnathanson.com>
2021-08-21 17:40:40 +0200

    check for blanks

--- frontend/admin/settings/user.php
+++ frontend/admin/settings/user.php
@@ -4,12 +4,11 @@
   Protect\with('/home/pi/solar-protocol/backend/protect/form.php','admin');
 
 
-  if(isset($_POST['hash']) && isset($_POST['rehash']) && $_POST['hash'] == $_POST['rehash'] && $_POST['hash'] != "" ) {
+  if(isset($_POST['hash']) && isset($_POST['rehash']) && $_POST['hash'] != "" ) {
 
-    updateUserInfo($_SESSION["username"], password_hash($_POST['hash'], PASSWORD_DEFAULT));
-
-
-
+    if(testInput){
+      updateUserInfo($_SESSION["username"], password_hash($_POST['hash'], PASSWORD_DEFAULT));
+    }
   } 
 
 
@@ -20,24 +19,42 @@ function updateUserInfo($un, $pwHash){
   try{
     $f = json_decode(file_get_contents($fileName),true);
     
-    var_dump($f);
+    //var_dump($f);
 
     $f['users'][$un]['hash']=$pwHash;
-    var_dump($f);
+    //var_dump($f);
     file_put_contents($fileName, json_encode($f, JSON_PRETTY_PRINT));
 
     echo "Password for user " . $_SESSION["username"] . " has been successfully changed.<br>";
   }
   catch(Exception $e) {
 
-    echo "Passwords do not match";
+    echo "Error";
 
   }
 }
 
 //what are the criteria to test the password?
 function testInput(){
+
+  //check that passwords match
+  if($_POST['hash'] != $_POST['rehash']){
+    echo "Passwords do not match";
+    return false;
+  }
+
+  //check for white spaces
+  if(strpos($_POST['hash'],' '){
+    return false;
+  }
+  
+  //check for characters
+  /*if ($_POST['hash']) {
     echo "Passwords can only contain letters, numbers, etc.";
+    return false; 
+  }*/
+
+  return true;
 }
 
 ?>
@@ -46,7 +63,7 @@ function testInput(){
 <html>
 <body>
 <p>
-  Submtting this form will change the password for the current user only on this server. If you have forgotten your previous password, contact Alex, Benedetta, or Tega for instructions to manually reset the password. If you have access to multiple servers, you will need to manually update your password on each one.
+  Submtting this form will change the password for the current user (<?php echo $_SESSION["username"]; ?>) only on this server. If you have forgotten your previous password, contact Alex, Benedetta, or Tega for instructions to manually reset the password. If you have access to multiple servers, you will need to manually update your password on each one.
 </p>
 
 <form method="POST" onsubmit="return confirm('Are you sure you want to change your password?');">

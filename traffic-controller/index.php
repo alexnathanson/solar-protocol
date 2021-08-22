55f77cf4cd8b6ba54a19a199dc049539aec9e5a6
alexnathanson <alex@alexnathanson.com>
2021-08-22 23:57:08 +0200

    foreach

--- traffic-controller/index.php
+++ traffic-controller/index.php
@@ -1,27 +1,36 @@
 <?php
 
-// this should be a dictionary of hashes tied to the server names for reference
-$serverHash = [];
+//in the future this should be either a database or a seperate json file
+$serverHash = [
+  "SPfA" => "",
+  "Hells Gate" => "",
+  "Tega" => "",
+  "Dominica" => "",
+  "Chile" => "",
+  "Caddie" => "",
+  "Low Carbon Methods" => ""
+];
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
   //check if key is correct
-  verifyPW($api_key)
+  verifyPW($_POST["api_key"],$_POST["ip"]);
 }
 
-function verifyPW($pw, $hash){
+function verifyPW($key, $ip){
 
 # hash generated from password_hash() more info at https://www.php.net/manual/en/function.password-hash.php
 
-#loop through all hashes...
-  if(password_verify($pw, $hash)){
-    updateIP();
+  #loop through all hashes...
+  foreach($serverHash as $name => $hash){
+    if(password_verify($key, $hash)){
+      updateIP($ip);
+    }
   }
-  return false;
 }
 
 //makes the API call to the DNS registry to update it
-function updateIP(){
+function updateIP($ip){
 
 }
 

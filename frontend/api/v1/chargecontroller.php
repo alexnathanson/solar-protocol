fbcdb243ae4dcac50f06c83838497728825cb0ee
alexnathanson <alex@alexnathanson.com>
2021-09-03 10:26:05 -0400

    ;

--- frontend/api/v1/chargecontroller.php
+++ frontend/api/v1/chargecontroller.php
@@ -225,7 +225,7 @@ if ($_SERVER["REQUEST_METHOD"] == "GET") {
       $fileName = "/home/pi/solar-protocol/backend/api/v1/deviceList.json";
 
       $contents = getFileContents($fileName);
-      echo gettype($contents)
+      echo gettype($contents);
 
       for ($d = 0; $d < count($contents);$d++;){
         array_push($output,$contents[$d]["name"]);

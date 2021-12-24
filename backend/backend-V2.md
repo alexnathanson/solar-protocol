674eccd7a5cd575ee4fd7bdd4e50e9d3a1b37ce4
AlexNathanson <alex@alexnathanson.com>
2021-12-24 11:36:51 -0500

    init timer

---
+++ backend/backend-V2.md
@@ -0,0 +1,4 @@
+# Backend V2
+
+api directory is being phased out. It is replaced with src.
+
---
+++ backend/src/solarProtocol.py
@@ -0,0 +1,152 @@
+'''
+Every server runs this script.
+This script retreives live PV power (watts) data from other servers.
+Compares data between devices and identifies the device producing the most power at the moment.
+If the local device is producing the most power, it becomes the Point of Entry (PoE) and updates the DNS system.
+Otherwise, the script changes nothing.
+'''
+
+
+dnsKey = ''
+
+'''
+possible values for dataValue (use "-" instead of spaces):
+PV current,PV power H,PV power L,PV voltage,
+battery percentage,battery voltage,charge current,
+charge power H,charge power L,date,load current,load power,load voltage,time
+possible values for apiValue include all of the above + scaled-wattage
+'''
+#this is the value to be retrieved locally (and then scaled). It could potentially be retrieved via an API call to itself, which might make code cleaner
+dataValue = 'PV power L'
+#this is the key to retrieve from remote devices
+apiValue = 'scaled-wattage'
+
+deviceList = "/home/pi/solar-protocol/backend/api/v1/deviceList.json"
+
+localDataFile = "/home/pi/solar-protocol/charge-controller/data/tracerData"+ str(datetime.date.today()) +".csv"
+
+logging.basicConfig(filename='/home/pi/solar-protocol/backend/api/v1/poe.log', level=logging.INFO)
+
+
+
+#return data from a particular server
+# this is redundant with the class... add this error handling to the class?
+def getData(dst, chosenApiValue):
+
+	try:
+		#returns a single value
+		response = requests.get('http://' + dst + '/api/v1/chargecontroller.php?value='+chosenApiValue, timeout = 5)
+		#print(response.text)		
+		#check if the response can be converted to a float
+		return float(response.text)
+	except requests.exceptions.HTTPError as err:
+		print(err)
+		return -1
+	except requests.exceptions.Timeout as err:
+		print(err)
+		return -1
+	except:
+		return -1
+
+def remoteData(dstIPs, chosenApiValue):
+	allData = []
+
+	for dst in dstIPs:
+		#print(dst)
+		allData.append(getData(dst, chosenApiValue))
+
+	# print("ALL DATA:")
+	# print(allData)
+
+	return allData
+	#determineServer(allData)
+
+def determineServer(remoteData,localData):
+
+	thisServer = True
+
+	#print(remotePVData)
+
+	#loop through data from all servers and compare scaled wattage
+	for s in remoteData:
+		if s > localData:
+			thisServer = False
+
+	if thisServer:
+		print('Point of entry')
+
+		logging.info(datetime.datetime.now())
+		
+		#print(SP.myIP)
+		#print(SP.getEnv('DNS_KEY'))
+
+		#getDNS(requests.get('http://whatismyip.akamai.com/').text)
+		SP.getRequest(SP.updateDNS(SP.myIP,str(SP.getEnv('DNS_KEY'))))
+
+	else:
+		print('Not point of entry')
+		#logging.info(datetime.datetime.now())#comment this out after testing
+
+#this should be added to class
+def localData(localDataFileCsv, chosenDataValue):
+
+	csvArray = []
+
+	#get the local PV data
+	with open(localDataFileCsv, mode='r') as csvfile:
+		localPVData = csv.reader(csvfile)
+
+		for row in localPVData:
+		 	csvArray.append(row)
+
+		#print(csvArray)
+
+		#loop through headers to determine position of value needed
+		for v in range(len(csvArray[0])):
+			if csvArray[0][v] == chosenDataValue:
+				return csvArray[-1][v]
+
+#this should be added to class
+def getIPList(deviceListJson, myMACAddr):
+
+	ipList = []
+
+	with open(deviceListJson) as f:
+	  data = json.load(f)
+
+	#print(data)
+
+	for i in range(len(data)):
+		#filter out local device's mac address
+		if str(data[i]['mac']).strip() !=  myMACAddr.strip():
+			ipList.append(data[i]['ip'])
+
+	#print(ipList)
+
+	return ipList
+
+def runSP():
+	myMAC = SP.getMAC(SP.MACinterface)
+	#print("my mac: " + myMAC)
+
+	localPVData = float(localData(localDataFile, dataValue)) * SP.pvWattsScaler()
+	print("My wattage scaled by " + str(SP.pvWattsScaler()) + ": " + str(localPVData))
+	remotePVData = remoteData(getIPList(deviceList, myMAC), apiValue)
+	#print("Remote Voltage: " + remotePVData)
+	determineServer(remotePVData, localPVData)
+
+if __name__ == '__main__':
+
+	import requests
+	import os
+	import fileinput
+	import json
+	import datetime
+	import csv
+	import logging
+	from SolarProtocolClass import SolarProtocol
+
+	#initialize SolarProtocolClass
+	SP = SolarProtocol()
+	
+	runSP()

42112d95848825d7e70c15412edc481cc87ec196
AlexNathanson <alex@alexnathanson.com>
2021-12-25 16:51:19 -0500

    fixed path

--- backend/createHTML/viz.py
+++ backend/createHTML/viz.py
@@ -466,7 +466,7 @@ def main():
     # alphaBlended2.save("clock1.png")
     #archive images
     archiveImage = Image.open(rootPath+"/frontend/images/clock.png")
-    archiveImage.save(rootPath+'/visualization/archive/clock-' + str(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")) +'.png') #archive plot
+    archiveImage.save(path+'/visualization/archive/clock-' + str(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")) +'.png') #archive plot
 
 
 if __name__ == "__main__":

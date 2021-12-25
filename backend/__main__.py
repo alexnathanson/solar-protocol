0dc9ae59c7ec94948c44f2444d45cc707a1c6074
AlexNathanson <alex@alexnathanson.com>
2021-12-24 19:26:28 -0500

    1 minute delay

--- backend/__main__.py
+++ backend/__main__.py
@@ -1,5 +1,12 @@
+#delay the start of the program for 1 minutes, because of an issues with the DNS resolver
+if __name__ != '__main__':
+	import time
+	time.sleep(60)
+
+
 import runner
 
 print("main!")
 
 runner.runSP()
+

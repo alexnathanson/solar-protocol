4cbebeef35195209062a7916d71d7b1f2d2526d5
AlexNathanson <alex@alexnathanson.com>
2021-12-25 16:10:50 -0500

    rem old dev list

--- utilities/setAllPermissions.sh
+++ utilities/setAllPermissions.sh
@@ -4,7 +4,6 @@ sudo chmod a+w /home/pi/local/.spenv
 sudo chmod a+w /home/pi/local/local.json
 sudo chmod a+w /home/pi/local/access.json
 sudo chmod -R a+w /home/pi/local/www
-sudo chmod a+w /home/pi/solar-protocol/backend/api/v1/deviceList.json
 sudo chmod a+w /home/pi/solar-protocol/backend/data/deviceList.json
 sudo chmod a+w /home/pi/solar-protocol/backend/data/poe.log
 sudo chmod +x /home/pi/solar-protocol/charge-controller/csv_datalogger.py

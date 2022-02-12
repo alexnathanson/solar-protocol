f503645c045b17192ddb43607a033761c1a707fc
AlexNathanson <alex@alexnathanson.com>
2022-02-12 14:52:31 -0500

    rm context

--- frontend/api/v1/opendata.php
+++ frontend/api/v1/opendata.php
@@ -386,7 +386,7 @@ function getServerData(){
     for ($d = 0; $d < count($ipList);$d++){
       //error_log('API destination: http://' . $ipList[$d] . $endPoint, 0);
 
-      array_push($output, json_decode(file_get_contents('http://' . $ipList[$d] . $endPoint, false, $streamContext)));
+      array_push($output, json_decode(file_get_contents('http://' . $ipList[$d] . $endPoint/*, false, $streamContext*/)));
     }
 
     echo json_encode($output);

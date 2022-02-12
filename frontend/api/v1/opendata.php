0ce392bfa1851845d0097601c2283d46b51d8854
AlexNathanson <alex@alexnathanson.com>
2022-02-12 14:01:40 -0500

    curl test

--- frontend/api/v1/opendata.php
+++ frontend/api/v1/opendata.php
@@ -367,8 +367,8 @@ function getServerData(){
    * the number is based on the order of the server names returned from the networkInfo=deviceList call
   **/
   if (is_numeric($_GET["server"])){
-    echo file_get_contents('http://' . $ipList[$_GET['server']] . $endPoint, 0);
-    //echo curlCall('http://' . $ipList[$_GET['server']] . $endPoint);
+    //echo file_get_contents('http://' . $ipList[$_GET['server']] . $endPoint, 0);
+    echo curlCall('http://' . $ipList[$_GET['server']] . $endPoint);
 
   } else if($_GET["server"] == "all"){
 

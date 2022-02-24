7620d78c19306fa7b92e6123a960ca3909c791f3
AlexNathanson <alex@alexnathanson.com>
2022-02-24 13:40:59 -0500

    fixed permissions

--- utilities/setAllPermissions.sh
+++ utilities/setAllPermissions.sh
@@ -4,9 +4,9 @@ sudo chmod a+w /home/pi/local/.spenv
 sudo chmod a+w /home/pi/local/local.json
 sudo chmod a+w /home/pi/local/access.json
 sudo chmod -R a+w /home/pi/local/www
+sudo chmod -R a+w /home/pi/local/data
 sudo chmod a+w /home/pi/solar-protocol/backend/data/deviceList.json
 sudo chmod a+w /home/pi/solar-protocol/backend/data/poe.log
-sudo chmod a+w /home/pi/solar-protocol/local/data
 sudo chmod +x /home/pi/solar-protocol/charge-controller/csv_datalogger.py
 sudo chmod a+w /home/pi/solar-protocol/frontend/index.html
 sudo chmod +x /home/pi/solar-protocol/utilities/update.sh

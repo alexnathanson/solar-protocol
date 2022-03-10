ce2f36c578c4115ff6fd58bbcf276ce30cd6ce25
AlexNathanson <alex@alexnathanson.com>
2022-03-10 12:20:13 -0500

    recurrsibe

--- utilities/setAllPermissions.sh
+++ utilities/setAllPermissions.sh
@@ -9,9 +9,9 @@ sudo chmod a+w /home/pi/solar-protocol/backend/data/deviceList.json
 sudo chmod a+w /home/pi/solar-protocol/backend/data/poe.log
 sudo chmod +x /home/pi/solar-protocol/charge-controller/csv_datalogger.py
 
-sudo chmod +x /home/pi/solar-protocol/backend/*.py
-sudo chmod +x /home/pi/solar-protocol/backend/createHTML/*.py
-sudo chmod +x /home/pi/solar-protocol/backend/core/*.py
+sudo chmod -R +x /home/pi/solar-protocol/backend
+sudo chmod -R +x /home/pi/solar-protocol/backend/createHTML
+sudo chmod -R +x /home/pi/solar-protocol/backend/core
 
 sudo chmod a+w /home/pi/solar-protocol/frontend/index.html
 sudo chmod +x /home/pi/solar-protocol/utilities/update.sh

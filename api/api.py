bf384d3873c977da8ba12158d814a3fa7c04d8fd
Jonathan Dahan <git@jonathan.is>
2022-10-18 18:46:01 -0400

    it fuckin works

--- api/api.py
+++ api/api.py
@@ -3,10 +3,10 @@ from typing import Union
 
 app = FastAPI(title="solar-protocol", docs_url="/docs")
 
-@app.get("/")
+@app.get("/api")
 def root():
     return {"message": "Hello World 👋"}
 
-@app.get("/items/{item_id}")
+@app.get("/api/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}

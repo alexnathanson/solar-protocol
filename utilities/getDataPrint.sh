aecf6263dee7c3d2739c6cd49c84a03e2db37193
Tega Brain <tega.brain@gmail.com>
2023-03-24 14:32:20 +1100

    Update getDataPrint.sh

--- utilities/getDataPrint.sh
+++ utilities/getDataPrint.sh
@@ -2,14 +2,18 @@
 
 # Run the first Python program and print its comments
 echo "Running clientPostIP"
-python3 /home/pi/solar-protocol/backend/core/clientPostIP.py | sed -n '/^\s*#/p'
+python3 /home/pi/solar-protocol/backend/core/clientPostIP.py
 
 # Run the second Python program and print its comments
 echo "Running getRemoteData"
-python3 /home/pi/solar-protocol/backend/core/getRemoteData.py | sed -n '/^\s*#/p'
+python3 /home/pi/solar-protocol/backend/core/getRemoteData.py
 
 # Run the third Python program and print its comments
 echo "Running solarProtocol.py"
-python3 /home/pi/solar-protocol/backend/core/solarProtocol.py | sed -n '/^\s*#/p'
+python3 /home/pi/solar-protocol/backend/core/solarProtocol.py
 
+echo "createHTML.py"
+python3 /home/pi/solar-protocol/backend/createHTML/createHTML.py
 
+echo "viz.py"
+python3 /home/pi/solar-protocol/backend/createHTML/viz.py

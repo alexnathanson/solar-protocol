98a3df8a1ed41385ecc8ebbbdda09dc73c38ac9b
Jonathan Dahan <git@jonathan.is>
2023-06-17 15:52:34 -0400

    read the register, pass the device

--- datalogger/csv_datalogger.py
+++ datalogger/csv_datalogger.py
@@ -72,7 +72,7 @@ def readFromDevice():
 
     data = dict(zip(fieldnames, registers[0:10]))
     data["timestamp"] = time.time()
-    data["battery percentage"] = toPercent(battery)
+    data["battery percentage"] = toPercent(battery[0])
     return data
 
 
